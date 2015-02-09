617ba67ce4e50fb5fe4835acc4cd519a5d199723
Matthias Koenig <konigmatt@googlemail.com>
2015-02-09 14:47:00 +0100

    First full version hepatocore wirtten

--- code/python/model-embedding/embedding/TransformHepatoCore.py
+++ code/python/model-embedding/embedding/TransformHepatoCore.py
@@ -21,16 +21,53 @@ hepatocore id <->
 @date: 2015-02-09
 '''
 #######################################################################
-gluconet = "../data/GlucoNet.xml"
-hepatocore = "../data/HepatoCore.xml"
+sbml_dir = "../data"
+gluconet = "{}/GlucoNet.xml".format(sbml_dir)
+hepatocore = "{}/HepatoCore.xml".format(sbml_dir)
 #######################################################################
+# Definition of compartments 
+comps_dict = [
+        {'id': 'extern', 'name':'extern'},
+        {'id': 'pm', 'name':'plasma membrane'},
+        {'id': 'cyto', 'name':'cytosol'},
+        {'id': 'mm', 'name': 'mitochondrial membrane'},
+        {'id': 'mim', 'name': 'mitochondrial inner membrane'},
+        {'id': 'mito', 'name':'mitochondrial matrix'},
+]
 
-import sys
+# mapping of hepatocore id parts to compartment ids
+comps_map = {
+        'blood_circulation' : 'extern',
+        'plasma_membrane' : 'pm',
+        'cytosol': 'cyto',
+        'mitochondrial_membrane' : 'mm',
+        'mitochondrial_inner_membrane' : 'mim',
+        'mitochondrial_matrix' : 'mito',
+} 
+
 import time
-import os
-import os.path
 from libsbml import *
- 
+
+def get_cid_from_string(s):
+    # return first entry in the comps map
+    for cid in comps_map.iterkeys():
+        if cid in s:
+            return cid
+    else:
+        print 'Not found compartment in: {}'.format(cid)
+    return None
+
+def replace_cid_in_string(s):
+    for cid, cid_new in comps_map.iteritems():
+        s = s.replace(cid, cid_new)
+    return s
+
+def remove_cid_from_name(s):
+    for cid in comps_map.iterkeys():
+        s = s.replace('_{}'.format(cid), '')
+    return s
+
+
 def check_sbml(filename): 
     current = time.clock();
     doc = readSBML(filename); 
@@ -59,31 +96,13 @@ def clone_and_fix_hepatocore():
     doc_old = readSBML(hepatocore)
     m_old = doc_old.getModel()
     
-    
-    # Definition of compartments for the unified model
-    comps = [
-        {'id': 'extern', 'name':'extern'},
-        {'id': 'pm',     'name':'plasma membrane'},
-        {'id': 'cyto',   'name':'cytosol'},
-        {'id': 'mm',     'name': 'mitochondrial membrane'},
-        {'id': 'mito',   'name':'mitochondrial matrix'},
-    ]
-    # mapping of id parts to compartment ids
-    comps_map = {
-        'cytosol': 'cyto',
-        'mitochondrial_matrix' : 'mito',
-        'blood_circulation' : 'extern',
-        'plasma_membrane' : 'pm',
-    } 
-             
-
-    # Create new fixed model
+    # Create new HepatoCore model with implemented fixes
     doc = SBMLDocument(3,1)
     m = SBMLDocument.createModel(doc, mid)
     m.setName(mid)
     
     # Create the compartments
-    for cinfo in comps:
+    for cinfo in comps_dict:
         c = m.createCompartment()
         c.setId(cinfo['id'])
         c.setName(cinfo['name'])
@@ -94,11 +113,52 @@ def clone_and_fix_hepatocore():
         # get the compartment from id
         # replace the id
         # replace name
-        s.setId(s_old.getId())
+        sid = s_old.getId()
+        cid = get_cid_from_string(sid)
+        # now replace
+        sid = replace_cid_in_string(sid)
+        cid = replace_cid_in_string(cid)
+        s.setId(sid)
+        s.setCompartment(cid)
+        name = s_old.getName()
+        if name:
+            name = remove_cid_from_name(name)
+            s.setName(name)
+     
+    # Create the reactions
+    for r_old in m_old.getListOfReactions():
+        r = m.createReaction()
         
+        rid = r_old.getId()
+        print '#', rid, '#'
+        cid = get_cid_from_string(rid)
+        # now replace
+        rid = replace_cid_in_string(rid)
+        cid = replace_cid_in_string(cid)
+        r.setId(rid)
+        r.setCompartment(cid)
+        name = r_old.getName()
+        if name:
+            name = remove_cid_from_name(name)
+            r.setName(name)   
         
-    
-    writeSBMLToFile(doc, '{}.xml'.format(mid) );
+        # substrates
+        print 'reactants:'
+        for s_old in r_old.getListOfReactants():
+            print s_old.getSpecies(), s_old.getStoichiometry()
+            s = r.createReactant()
+            s.setSpecies(replace_cid_in_string(s_old.getSpecies()))
+            s.setStoichiometry(s_old.getStoichiometry())
+        
+        # products
+        print 'products:'
+        for s_old in r_old.getListOfProducts():
+            print s_old.getSpecies(), s_old.getStoichiometry()
+            s = r.createProduct()
+            s.setSpecies(replace_cid_in_string(s_old.getSpecies()))
+            s.setStoichiometry(s_old.getStoichiometry())
+        
+    writeSBMLToFile(doc, '{}/{}.xml'.format(sbml_dir, mid) );
     return m
 
 
