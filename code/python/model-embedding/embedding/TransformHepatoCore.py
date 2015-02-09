c7220a5781427dca46aa72f6f4dee638cd91caf6
Matthias Koenig <konigmatt@googlemail.com>
2015-02-09 12:25:03 +0100

    Recoding Hepatocore started

--- code/python/model-embedding/embedding/TransformHepatoCore.py
+++ code/python/model-embedding/embedding/TransformHepatoCore.py
@@ -46,9 +46,68 @@ def check_sbml(filename):
     return errors;
 
 
+def clone_and_fix_hepatocore():
+    ''' 
+        Clones the elements from the network and fixes the SBML
+        * Compartment creation
+        * Compartment mapping
+        * Fix compartment part of ID 
+    '''
+    version = 1
+    mid = 'HepatoCore2_v{}'.format(version)
+    # read the original model
+    doc_old = readSBML(hepatocore)
+    m_old = doc_old.getModel()
+    
+    
+    # Definition of compartments for the unified model
+    comps = [
+        {'id': 'extern', 'name':'extern'},
+        {'id': 'pm',     'name':'plasma membrane'},
+        {'id': 'cyto',   'name':'cytosol'},
+        {'id': 'mm',     'name': 'mitochondrial membrane'},
+        {'id': 'mito',   'name':'mitochondrial matrix'},
+    ]
+    # mapping of id parts to compartment ids
+    comps_map = {
+        'cytosol': 'cyto',
+        'mitochondrial_matrix' : 'mito',
+        'blood_circulation' : 'extern',
+        'plasma_membrane' : 'pm',
+    } 
+             
+
+    # Create new fixed model
+    doc = SBMLDocument(3,1)
+    m = SBMLDocument.createModel(doc, mid)
+    m.setName(mid)
+    
+    # Create the compartments
+    for cinfo in comps:
+        c = m.createCompartment()
+        c.setId(cinfo['id'])
+        c.setName(cinfo['name'])
+    
+    # Create the species
+    for s_old in m_old.getListOfSpecies():
+        s = m.createSpecies()
+        # get the compartment from id
+        # replace the id
+        # replace name
+        s.setId(s_old.getId())
+        
+        
+    
+    writeSBMLToFile(doc, '{}.xml'.format(mid) );
+    return m
+
 
 
 if __name__ == "__main__":
+    # Do all the replacements to the model
+    m2 = clone_and_fix_hepatocore()
+    exit() 
+    
     print '# HepatoCore #'
     # check_sbml(hepatocore)
     doc = readSBML(hepatocore)
@@ -59,19 +118,19 @@ if __name__ == "__main__":
         
     # species dictionary
     for s in Model.getListOfSpecies(m):
-        print s.getId(), s.getName(), s.getCompartment(), s.getBoundaryCondition()
+        print s.getId(), s.getName(), s.getCompartment()
         
     # species dictionary
     for r in Model.getListOfReactions(m):
-        print r.getId(), r.getName(), r.getCompartment(), r.getBoundaryCondition()
+        print r.getId(), r.getName()
     
+    print
+    print '# GlucoNet #'
+    doc = readSBML(gluconet)
+    m2 = doc.getModel()
+    # compartment dictionary
+    for c in Model.getListOfCompartments(m2):
+        print c.getId(), c.getName()
         
-    
-    
-    
-    
-    
-    
-    # print compartments
-    print m.getId()
-    print m.getName()
+    print '#' * 80
+   
